f83f084aa09f3043b5e2e8c11e13e223db838d82
tiagoggdev <santiagogarciagarzon.8@gmail.com>
2025-10-05 23:50:48 -0500

    Se ajustan el nombre de las paginas

--- DigitalBankAssessment-frontend/src/app/app.routes.ts
+++ DigitalBankAssessment-frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { CreateUserComponent } from './features/user/pages/create-user/create-us
 import { ListUsersComponent } from './features/user/pages/list-users/list-users.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'usuario/listar', pathMatch: 'full' },
+  { path: '', redirectTo: 'usuario/crear', pathMatch: 'full' },
   { path: 'usuario/crear', component: CreateUserComponent },
   { path: 'usuario/listar', component: ListUsersComponent }
 ];
